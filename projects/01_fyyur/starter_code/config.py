b98861f9ffea3dd381b61bad3b366eb1cb3a7089
Lucidio <lucidiovacas@gmail.com>
2020-04-04 20:49:58 +0200

    Fixing Label artist

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -6,9 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
+# DONE IMPLEMENT DATABASE URL
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:EresTonto@localhost:5432/fyyur'
